52814691f9c1b636292532f5457d9e4f9cea3eaf
y.okamoto <okamoto.yuuki.1113+github@gmail.com>
2021-09-18 17:59:25 +0900

    数値型について勉強

--- kinocode/Greeting.go
+++ kinocode/Greeting.go
@@ -1,9 +1,77 @@
 package main
-import ("fmt")
+import (
+	"fmt"
+	"reflect"
+)
 
 func main(){
 	fmt.Println("Good Morning!")
 	fmt.Println("Good Afternoon!")
 	fmt.Println("Good Evening!")
+
+	// 変数名を再度宣言することはできないので、変数名を分けている
+	var num1 int = 1
+	fmt.Println(num1)
+	var num2 = 2
+	fmt.Println(num2)
+	num3 := 3
+	fmt.Println(num3)
+
+	// case sensitive (大文字・小文字は区別される)
+	NUM := 4
+	fmt.Println(NUM)
+	Num := 5
+	fmt.Println(Num)
+
+	// Integer
+	// https://blog.y-yuki.net/entry/2017/04/27/000000
+	// int8 は 8bit なので、0~255までの値を取るが、これは -128 ~ 127 の値を取る。
+	// 1bit目は、プラス・マイナス符号になる。
+	// -128, +127を超えると「overflows int8」となる
+	fmt.Println("========== int8について ==========")
+	var int8_num int8 = 127
+	fmt.Println(int8_num)
+	fmt.Println(reflect.TypeOf(int8_num))
+
+	// uint8は 8bit でプラスの値のみ。 0~255までの値を取る。
+	fmt.Println("========== uint8について ==========")
+	var uint8_num uint8 = 255
+	fmt.Println(uint8_num)
+	fmt.Println(reflect.TypeOf(uint8_num))
+
+	// byte は uint8 のエイリアス
+	fmt.Println("========== byteについて ==========")
+	var byte_num byte = 255
+	fmt.Println(byte_num)
+	fmt.Println(reflect.TypeOf(byte_num))
+
+	// int16 は 16bit -32768 ~ +32767 の値を取る。
+	fmt.Println("========== int16について ==========")
+	var int16_num int16 = 32767
+	fmt.Println(int16_num)
+	fmt.Println(reflect.TypeOf(int16_num))
+
+	// int32 は -2147483648 ~ +2147483647 の値を取る。
+	fmt.Println("========== int32について ==========")
+	var int32_num int32 = 2147483647
+	fmt.Println(int32_num)
+	fmt.Println(reflect.TypeOf(int32_num))
+
+	// int64 は -9223372036854775808 ~ +9223372036854775807 の値を取る。
+	fmt.Println("========== int64について ==========")
+	var int64_num int64 = 9223372036854775807
+	fmt.Println(int64_num)
+	fmt.Println(reflect.TypeOf(int64_num))
+
+	// Float
+	// 整数と小数点で合計8桁目までしか表示しない
+	fmt.Println("========== float32について ==========")
+	var float32_num float32 = 1.234567890543298765432456789087654356789864568
+	fmt.Println(float32_num)
+
+	// 整数と小数点で合計17桁目までしか表示しない
+	fmt.Println("========== float64について ==========")
+	var float64_num float64 = 1.12346789098321235678909412345678987654321234567
+	fmt.Println(float64_num)
 }
 
