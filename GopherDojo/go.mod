66a15dd78e02f4bfdc1c37e7227517207048b299
y.okamoto <okamoto.yuuki.1113+github@gmail.com>
2021-09-27 20:44:18 +0900

    Mckage導入してみる

---
+++ GopherDojo/go.mod
@@ -0,0 +1,5 @@
+module golang_lessons/GopherDojo
+
+go 1.17
+
+require github.com/tenntenn/greeting v1.0.0
---
+++ GopherDojo/lesson01.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/tenntenn/greeting"
+)
+
+func main() {
+	greeting.Do()
+}
