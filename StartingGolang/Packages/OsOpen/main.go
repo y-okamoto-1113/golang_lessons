83e565243a7149461a4b848f4bb3ac06324120fb
y.okamoto <okamoto.yuuki.1113+github@gmail.com>
2021-10-24 00:20:56 +0900

    learned about os.Open, Read methods

---
+++ StartingGolang/Packages/OsOpen/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("test.txt")
+	fmt.Println("file =>", file)
+	fmt.Println("err =>", err)
+	defer file.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	buff := make([]byte, 1024)
+	fmt.Println("buff 111=>", buff)
+	count, err := file.Read(buff) // byte型の変数を定義して、そこにデータを突っ込んでる。byte型で定義した capacity 以上は代入できない。 countは文字数を返す。
+	fmt.Println("buff 222=>", buff)
+	fmt.Println("count =>", count)
+	fmt.Println("err =>", err)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(buff))
+
+}
